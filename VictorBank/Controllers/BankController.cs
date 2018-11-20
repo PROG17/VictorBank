446d06a85be0dd1b1b02a67b34fc588d04426c39
Victor Hermansson <hermansson_79@hotmail.com>
2018-11-20 16:30:08 +0100

    Added tests, cutomers and accounts

--- VictorBank/Controllers/BankController.cs
+++ VictorBank/Controllers/BankController.cs
@@ -13,13 +13,39 @@ namespace VictorBank.Controllers
 
         public BankController()
         {
-            _repository = new BankRepository();
+            _repository = BankRepository.Instance();
         }
 
         public IActionResult CustomerAccounts()
         {
             var customerAccounts = _repository.GetAllCustomerAndAccounts();
             return View(customerAccounts);
+        }
+
+        public IActionResult DepositWithdraw(string message)
+        {
+            ViewBag.message = message;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Deposit(int accountNumber, int sum)
+        {
+            if (sum == 0 || accountNumber == 0)
+                return RedirectToAction("DepositWithdraw", new { message = "Summan eller kontot är i fel format eller 0" });
+            var result = _repository.Deposit(accountNumber, sum);
+            return RedirectToAction("DepositWithdraw", new { message = result });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Withdraw(int accountNumber, int sum)
+        {
+            if (sum == 0 || accountNumber == 0)
+                return RedirectToAction("DepositWithdraw", new { message = "Summa eller konto är i fel format eller 0" });
+            var result = _repository.Withdraw(accountNumber, sum);
+            return RedirectToAction("DepositWithdraw", new { message = result });
         }
     }
 }
